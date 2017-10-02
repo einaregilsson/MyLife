e5ba2bb7692eb27b59a3a744de22cb909a1e620f
Einar Egilsson <einar@einaregilsson.com>
2017-10-02 14:59:28 +0000

    Update Dropbox backup to use Dropbox API v2

--- handlers/dropbox.py
+++ handlers/dropbox.py
@@ -25,10 +25,6 @@ class DropboxBackupHandler(webapp2.RequestHandler):
 				self.log('No access token available, no backup will be performed.')
 				return
 
-			headers = {
-				'Content-Type' : 'application/json',
-				'Authorization' : 'Bearer ' + settings.dropbox_access_token
-			}
 
 			posts = [p for p in Post.query().order(Post.date).fetch()]
 
@@ -40,28 +36,36 @@ class DropboxBackupHandler(webapp2.RequestHandler):
 				post_text.write(p.text.replace('\r\n', '\n').replace('\n', '\r\n').rstrip())
 				post_text.write('\r\n\r\n')
 
-			result = self.put_file(headers, 'MyLife.txt', post_text.getvalue().encode('utf-8'))
+			result = self.put_file(settings.dropbox_access_token, 'MyLife.txt', post_text.getvalue().encode('utf-8'))
 			post_text.close()
-			self.log('Backed up posts. Revision: %s' % result['revision'])
-
-			result = self.get_dropbox_filelist(settings.dropbox_access_token, headers)
+			self.log('Backed up posts. Revision: %s' % result['rev'])
 
 			self.log('Fetching Dropbox file list')
 			
-			files_in_dropbox = [m['path'][1:] for m in result['contents']]
+			files_in_dropbox = self.get_dropbox_filelist(settings.dropbox_access_token)
 			
+			self.log('Got %s files from Dropbox' % len(files_in_dropbox))
+
 			self.log('Fetching images...')
 			images = [i for i in UserImage.query().order(UserImage.date).fetch()]
 
-			images = [i for i in images if not i.filename in files_in_dropbox or not i.backed_up_in_dropbox]
+			self.log('Total images in MyLife: %s' % len(images))
+
+			not_backed_up = [i for i in images if not i.backed_up_in_dropbox]
+			not_in_dropbox = [i for i in images if not i.filename in files_in_dropbox]
+
+			self.log('\nFiles not backed up: \n\n' + '\n'.join([i.filename for i in not_backed_up]))
+			self.log('\nFiles marked as backed up, but not in Dropbox: \n\n' + '\n'.join([i.filename for i in not_in_dropbox]))
+
+			images = not_backed_up + not_in_dropbox
 
 			images_total = len(images)
 			self.log('Found %s images that need to be backed up in Dropbox' % images_total)
 			for img in images:
 				self.log('Backing up %s' % img.filename)
 				bytes = filestore.read(img.original_size_key)
-				result = self.put_file(headers, img.filename, bytes)
-				self.log('Backed up %s. Revision: %s' % (img.filename, result['revision']))
+				result = self.put_file(settings.dropbox_access_token, img.filename, bytes)
+				self.log('Backed up %s. Revision: %s' % (img.filename, result['rev']))
 				img.backed_up_in_dropbox = True
 				img.put()
 				images_backed_up += 1
@@ -76,6 +80,8 @@ class DropboxBackupHandler(webapp2.RequestHandler):
 				'The next time it runs it will continue backing up your posts and images.' +
 				'%s images out of %s were backed up before failing' % (images_backed_up, images_total))
 		except Exception, ex:
+			self.log('Failed to backup posts and images to dropbox: %s' % traceback.format_exc(6))
+			logging.exception("message")
 			self.log('ERROR: %s' % ex)
 			log_error('Error backing up to Dropbox', 'Failed to backup posts and images to dropbox: %s' % traceback.format_exc(6))
 
@@ -84,31 +90,106 @@ class DropboxBackupHandler(webapp2.RequestHandler):
 		self.response.write(str(msg) + '\r\n')
 		logging.info(msg)
 
-	def put_file(self, headers, name, bytes):
+	def get_file_info(self, access_token, name):
 
-		params = {
-
+		headers = {
+			'Content-Type' : 'application/json',
+			'Authorization' : 'Bearer ' + access_token
 		}
 
-		qs = '&'.join('%s=%s' % (k,v) for k,v in params.items())
+		data = {
+    		"path": "/" + name,
+		    "include_media_info": False,
+		    "include_deleted": False,
+		    "include_has_explicit_shared_members": False
+		}
+
+		result = urlfetch.fetch(
+			payload=json.dumps(data),
+			method=urlfetch.POST,
+			url='https://api.dropboxapi.com/2/files/get_metadata',
+			headers=headers
+		)
+
+		if result.status_code != 200:
+			raise Exception("Failed to get file metadata from Dropbox. Status: %s, body: %s" % (result.status_code, result.content))
+		self.log(result.content)
+		return json.loads(result.content)
+
+
+	def put_file(self, access_token, name, bytes):
+
+#		info = self.get_file_info(access_token, name)
+#		self.log(info)
+
+		dropbox_args = {
+    		"path": "/" + name,
+    		"mode": { ".tag" : "overwrite"},
+    		"autorename": True,
+    		"mute": False
+		}
+
+		headers = {
+			'Content-Type' : 'application/octet-stream',
+			'Authorization' : 'Bearer ' + access_token,
+			'Dropbox-API-Arg' : json.dumps(dropbox_args)
+		}
 
 		result = urlfetch.fetch(
 			payload=bytes,
 			method=urlfetch.POST,
-			url='https://api-content.dropbox.com/1/files_put/auto/' + name + '?' + qs,
+			url='https://content.dropboxapi.com/2/files/upload',
 			headers=headers
-			)
+		)
+
 		if result.status_code != 200:
+			self.log(result.content)
 			raise Exception("Failed to send file to Dropbox. Status: %s, body: %s" % (result.status_code, result.content))
 		return json.loads(result.content)
 
 
-	def get_dropbox_filelist(self, access_token, headers):
+	def get_dropbox_filelist(self, access_token):
+		headers = {
+			'Content-Type' : 'application/json',
+			'Authorization' : 'Bearer ' + access_token
+		}
+
+		data = {
+			"path": "",
+			"recursive": True,
+			"include_media_info": False,
+			"include_deleted": False,
+			"include_has_explicit_shared_members": False,
+			"include_mounted_folders": False,
+			"limit" : 1000
+		}
+
 		result = urlfetch.fetch(
-			url='https://api.dropbox.com/1/metadata/auto/',
+			payload=json.dumps(data),
+			method=urlfetch.POST,			
+			url='https://api.dropboxapi.com/2/files/list_folder',
     		headers=headers)
+
 		if result.status_code != 200:
-			raise Exception("Failed to send file to Dropbox. Status: %s, body: %s" % (result.status_code, result.content))
+			raise Exception("Failed to get files from Dropbox. Status: %s, body: %s" % (result.status_code, result.content))
 		
-		return json.loads(result.content)		
+		json_data = json.loads(result.content)
+		file_list = [o['name'] for o in json_data['entries']]
+
+		#Get everything
+		while json_data['has_more']:
+			self.log('Getting next batch...')
+			result = urlfetch.fetch(
+				payload=json.dumps({"cursor" : json_data['cursor']}),
+				method=urlfetch.POST,			
+				url='https://api.dropboxapi.com/2/files/list_folder/continue',
+	    		headers=headers)
+
+			if result.status_code != 200:
+				raise Exception("Failed to get files from Dropbox. Status: %s, body: %s" % (result.status_code, result.content))
+
+			json_data = json.loads(result.content)
+			file_list.extend([o['name'] for o in json_data['entries']])
+
+		return file_list	
 
